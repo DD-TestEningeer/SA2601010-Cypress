578cfa37240c00def988a7e9ea81b3991e290093
DD-TestEningeer <22testengineer@gmail.com>
2026-05-29 22:32:05 +0530

    day1

---
+++ cypress/e2e/1_open_url.cy.js
@@ -0,0 +1,38 @@
+
+
+// step 1 - adding test group 
+describe("Test Group/Module/Suite 1 ",  ()=>{ 
+
+
+// step 2 - add first test 
+it("This is my  first test case", ()=>{
+
+// step 3 - provide the actual test case steps 
+
+// open the browser and navigate to the url 
+
+cy.visit("https://www.google.com");
+
+// assert the page title 
+
+// assert the page url 
+
+// enter the user 
+// enter the pass
+
+
+
+
+} );
+
+
+
+}
+
+
+
+
+
+
+
+ );
---
+++ cypress/e2e/2_open_amazon.cy.js
@@ -0,0 +1,27 @@
+
+
+
+
+// test group 
+
+describe("Amazon test group ", ()=>{
+
+// test case 
+
+it("open amazon homepage", ()=>{
+
+// test steps 
+
+// open amazon page 
+
+cy.visit("https://www.amazon.in");
+
+
+
+
+
+});
+
+
+
+});
